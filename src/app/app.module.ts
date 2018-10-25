4752d7994cb5c937674ee7280484fe21f5f47db4
evagolabek <golabek.eva@gmail.com>
2018-10-25 22:48:19 +0200

    add and delete card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { CustomerDetailsComponent } from './customer-details/customer-details.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     CustomerListComponent,
-    CustomerDetailsComponent
+    CustomerDetailsComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
